910518c780947fb94afc4efe83e40ed950e36a0a
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-04 16:16:23 -0500

    created albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,6 +4,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
    user_id INT NOT NULL AUTO_INCREMENT,
-   artist
+   artist VARCHAR(50) DEFAULT 'NONE',
+   name VARCHAR(100) NOT NULL,
+   release_date INT,
+   sales DOUBLE,
+   genre VARCHAR(100) NOT NULL,
    PRIMARY KEY (user_id)
 );
