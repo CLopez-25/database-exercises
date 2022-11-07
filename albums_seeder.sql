edb942f310df263b70a7bfcaf8c1d39061024026
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-07 13:33:31 -0600

    completed 1-3 parts of basic commands lect

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,9 +1,9 @@
 USE codeup_test_db;
 
-INSERT INTO albums (artist, name, release_date, sales, genre)
+INSERT INTO albums (artist_name, album_name, release_date, sales, genre)
 VALUES ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop'),
        ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2, 'soft rock'),
        ('Eagles', 'Hotel California', 1976, 31.8, 'Soft rock'),
        ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard rock'),
-       ('Led Zepplin', 'Led Zeppplin IV', 1971, 30.4, 'Hard rock'),
-       ('Shania Twain', 'Come On Over', 1997, 30.4, 'Country')
+       ('Led Zepplin', 'Led Zepplin IV', 1971, 30.4, 'Hard rock'),
+       ('Shania Twain', 'Come On Over', 1997, 30.4, 'Country');
