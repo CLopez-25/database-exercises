b827ce24e1a1f6de2e54a671e504dd1974b6536f
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-08 13:52:10 -0600

    completed part two of where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -15,6 +15,9 @@ OR first_name = 'Vidya'
 OR first_name = 'Maya'
     AND gender = 'M';
 
-SELECT * FROM employees WHERE last_name LIKE ('E%')
-    OR last_name LIKE ('%E');
+SELECT * FROM employees WHERE last_name LIKE ('E%') OR last_name LIKE ('%E');
 
+SELECT * FROM employees WHERE last_name LIKE ('E%') AND last_name LIKE ('%E');
+
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
