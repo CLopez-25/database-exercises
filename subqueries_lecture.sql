3faf337dcd24844aad66f2e8629c20d59b590a5b
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-14 11:37:35 -0600

    deleted lines in subq lect

--- subqueries_lecture.sql
+++ subqueries_lecture.sql
@@ -5,9 +5,6 @@
 #   WHERE emp_no IN (SELECT emp_no FROM dept_manager);
 
 
-# SELECT dept_name FROM departments WHERE dept_no IN (SELECT dept_no FROM dept_manager WHERE emp_no IN (SELECT emp_no FROM employees WHERE gender = 'F') AND to_date > NOW() AND emp_no IN (SELECT emp_no FROM employees WHERE gender = 'F'));
-
-
 USE codeup_test_db;
 
 INSERT INTO preferences (person_id, album_id) VALUES ((SELECT person_id FROM persons WHERE first_name = 'Tareq'), (SELECT album_id FROM albums WHERE album_name = 'Led Zeppelin IV'));
