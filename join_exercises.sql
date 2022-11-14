5ba84750a5fd4f9d849b7ad913c94d7f750ede24
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-14 09:56:29 -0600

    completed join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -9,6 +9,7 @@ FROM employees as e
               ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
+
 # START OF EXERCISES:
 # Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department:
 SELECT d.dept_name AS Deparment_Name, CONCAT(e.first_name, ' ', e.last_name) AS Deparment_Manager
@@ -18,6 +19,7 @@ JOIN departments as d ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
+
 # Find the name of all departments currently managed by women.
 SELECT d.dept_name AS Deparment_Name, CONCAT(e.first_name, ' ', e.last_name) AS Deparment_Manager
 FROM employees as e
@@ -26,15 +28,24 @@ JOIN departments as d ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01' AND gender = 'F'
 ORDER BY d.dept_name;
 
+
 # Find the current titles of employees currently working in the Customer Service department.
-SELECT 
-
-
-
+SELECT title,
+       COUNT(title)
+FROM titles t
+    JOIN employees e on e.emp_no = t.emp_no
+    JOIN dept_emp de on e.emp_no = de.emp_no
+WHERE de.dept_no LIKE '%9'
+  AND t.to_date LIKE '%9'
+  AND de.to_date LIKE '%9'
+GROUP BY title
+ORDER BY COUNT(title) DESC;
 
 
 # Find the current salary of all current managers.
-SELECT DISTINCT d.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Deparment_Manager, s.salary AS Salary
+SELECT DISTINCT d.dept_name AS Department_Name,
+    CONCAT(e.first_name, ' ', e.last_name) AS Deparment_Manager,
+    s.salary AS Salary
 FROM employees as e
 JOIN dept_manager as de ON de.emp_no = e.emp_no
 JOIN departments as d ON d.dept_no = de.dept_no
